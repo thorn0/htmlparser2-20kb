5472fd7d861b87dfff9df407faa467d66c6f8f8b
Georgii Dolzhykov <thorn.mailbox@gmail.com>
2018-09-05 15:23:46 +0300

    add UMD boilerplate

--- transform3.js
+++ transform3.js
@@ -1,9 +1,27 @@
 module.exports = (fileInfo, { jscodeshift: j }) => {
     const ast = j(fileInfo.source);
 
+    // remove leftovers after transform2
+
     ast.find(j.AssignmentExpression, {
         left: { property: { name: 'defineProperty' } }
     }).remove();
 
-    return ast.toSource();
+    // add UMD boilerplate
+
+    const source = ast.toSource().replace(
+        /[\w$]+\.HTMLPARSER2\$TMP\$GLOBAL = ([\w$]+);/,
+        `if (typeof exports === "object" && typeof module !== "undefined") {
+            // CommonJS
+            module.exports = $1;
+        } else if (typeof define === "function" && define.amd) {
+            // RequireJS
+            define(function() { return $1; });
+        } else {
+            // <script>
+            this.html = $1;
+        }`
+    );
+
+    return source;
 };
