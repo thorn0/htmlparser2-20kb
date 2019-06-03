f5b1b1482dfc9073c74c037acd1dfb03f532db84
Georgii Dolzhykov <thorn.mailbox@gmail.com>
2019-06-03 20:52:35 +0300

    tweaks

--- transform/transform2.js
+++ transform/transform2.js
@@ -33,5 +33,27 @@ module.exports = (fileInfo, { jscodeshift: j }) => {
       p.value.arguments[0] = j.literal(p.value.arguments[0].value.replace(/!$/, ''));
     });
 
-  return ast.toSource();
+  // add UMD boilerplate
+
+  const source = ast
+    .toSource()
+    .replace('\n', '\n  var hop = Object.prototype.hasOwnProperty, isArray = Array.isArray;\n')
+    .replace(/Object\.prototype\.hasOwnProperty\./g, 'hop.')
+    .replace(/Array\.isArray\(/g, 'isArray(')
+    .replace(/([\w.]+) = (\w+);\s+([\w.]+) = \2;/g, '$1 = $3 = $2;')
+    .replace(
+      /[\w$]+\.HTMLPARSER2\$TMP\$GLOBAL = ([\w$]+);/,
+      `if (typeof exports === "object" && typeof module !== "undefined") {
+    // CommonJS
+    module.exports = $1;
+  } else if (typeof define === "function" && define.amd) {
+    // RequireJS
+    define(function() { return $1; });
+  } else {
+    // <script>
+    this.htmlparser = $1;
+  }`
+    );
+
+  return source;
 };
