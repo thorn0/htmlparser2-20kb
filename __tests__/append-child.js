347ab4d87891af7b1fbb5009ea5f7e0c64dd964e
Georgii Dolzhykov <thorn.mailbox@gmail.com>
2023-11-18 01:43:29 +0200

    v3.10.11: fix appendChild broken in 3.10.10; remove node before append/prepend child only if dom is passed

--- __tests__/append-child.js
+++ __tests__/append-child.js
@@ -1,6 +1,17 @@
 const htmlparser = require("../dist/htmlparser2-20kb");
 
 describe("htmlparser.appendChild", () => {
+  test("basic test", () => {
+    const dom = htmlparser.parse("<a><b></b></a>");
+    const a = dom[0];
+    htmlparser.appendChild(a, htmlparser.create("i"));
+    expect(dom.length).toBe(1);
+    expect(dom[0]).toBe(a);
+    expect(a.next).toBe(null);
+    expect(a.children.length).toBe(2);
+    expect(a.children[1].name).toBe("i");
+  });
+
   test("3 args", () => {
     const dom = htmlparser.parse("<a></a><b></b>");
     const a = dom[0];
